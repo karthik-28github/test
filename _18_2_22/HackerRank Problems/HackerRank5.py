c3c7b4427713538cc835c1052fe125b90d265e67
karthik B <karthik.balakrishna@modernchipsolutions.com>
2022-02-18 17:58:23 +0530

    18/2/22

---
+++ _18_2_22/HackerRank Problems/HackerRank5.py
@@ -0,0 +1,78 @@
+'''
+itertools.combinations(iterable, r)
+This tool returns the  length subsequences of elements from the input iterable.
+
+Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
+
+Sample Code
+
+>>> from itertools import combinations
+>>>
+>>> print list(combinations('12345',2))
+[('1', '2'), ('1', '3'), ('1', '4'), ('1', '5'), ('2', '3'), ('2', '4'), ('2', '5'), ('3', '4'), ('3', '5'), ('4', '5')]
+>>>
+>>> A = [1,1,3,3,3]
+>>> print list(combinations(A,4))
+[(1, 1, 3, 3), (1, 1, 3, 3), (1, 1, 3, 3), (1, 3, 3, 3), (1, 3, 3, 3)]
+Task
+
+You are given a string .
+Your task is to print all possible combinations, up to size , of the string in lexicographic sorted order.
+
+Input Format
+
+A single line containing the string  and integer value  separated by a space.
+
+Constraints
+
+
+The string contains only UPPERCASE characters.
+
+Output Format
+
+Print the different combinations of string  on separate lines.
+
+Sample Input
+
+HACK 2
+Sample Output
+
+A
+C
+H
+K
+AC
+AH
+AK
+CH
+CK
+HK
+'''
+import itertools
+n, m = input().split()
+m1=int(m)
+lst=list(n)
+lst=sorted(lst,reverse=False)
+per_lst=[]
+
+def permu(lst,i):
+        lst1=[]
+        perm=itertools.combinations(lst,i)
+        lst1.extend(perm)
+        return lst1
+def join_sort(lst_perm1):
+    lst_join=[]
+    for i2 in lst_perm1:
+        str1=''.join(i2)
+        lst_join.append(str1)
+    sort_lst_join=sorted(lst_join,reverse=False)
+    return sort_lst_join
+
+result=[]
+for i in range(1,m1+1):
+    perm1=permu(lst,i)
+    res1=join_sort(perm1)
+    result.extend(res1)
+
+for i3 in result:
+    print(i3)
